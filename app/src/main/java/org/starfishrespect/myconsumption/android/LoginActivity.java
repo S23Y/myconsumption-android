c5a4efb1abc76ed3660e20730075138959df8b9b
Thibaud Ledent <thibaudledent@gmail.com>
2015-02-25 10:18:12 +0100

    adding res files

--- app/src/main/java/org/starfishrespect/myconsumption/android/LoginActivity.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/LoginActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import org.starfishrespect.myconsumption.android.asynctasks.GetUserAsyncTask;
+import org.starfishrespect.myconsumption.android.dao.DatabaseHelper;
+import org.starfishrespect.myconsumption.android.data.KeyValueData;
 
 /**
  * Login Activity
--- app/src/main/java/org/starfishrespect/myconsumption/android/MainActivity.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/MainActivity.java
@@ -5,18 +5,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
@@ -134,8 +129,7 @@ public class MainActivity extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+            return inflater.inflate(R.layout.fragment_main, container, false);
         }
 
         @Override
