cfb108a6f0e53454fc0640d3a459d8355b0b7d78
Thibaud Ledent <thibaudledent@gmail.com>
2015-04-03 16:51:23 +0200

    trying to add sliding tab in StatActivity

--- app/src/main/java/org/starfishrespect/myconsumption/android/ui/SlidingStatFragment.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/ui/SlidingStatFragment.java
@@ -1,24 +1,47 @@
-//package org.starfishrespect.myconsumption.android.ui;
-//
-//import android.os.Bundle;
-//import android.support.v4.app.Fragment;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import org.starfishrespect.myconsumption.android.R;
-//
-///**
-// * Created by thibaud on 30.03.15.
-// */
-//public class SlidingStatFragment extends Fragment {
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        ViewGroup rootView = (ViewGroup) inflater.inflate(
-//                R.layout.fragment_sliding_stat, container, false);
-//
-//        return rootView;
-//    }
-//}
+package org.starfishrespect.myconsumption.android.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.starfishrespect.myconsumption.android.R;
+
+/**
+* Created by thibaud on 30.03.15.
+*/
+public class SlidingStatFragment extends Fragment {
+
+    private static final String ARG_POSITION = "position";
+
+    TextView mTextView;
+
+    private int position;
+
+    public static SlidingStatFragment newInstance(int position) {
+        SlidingStatFragment f = new SlidingStatFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        f.setArguments(b);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        position = getArguments().getInt(ARG_POSITION);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_sliding_stat,container,false);
+        mTextView = (TextView) rootView.findViewById(R.id.textView);
+        ViewCompat.setElevation(rootView, 50);
+        mTextView.setText("CARD " + position);
+        return rootView;
+    }
+}
--- app/src/main/java/org/starfishrespect/myconsumption/android/ui/StatActivity.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/ui/StatActivity.java
@@ -1,19 +1,19 @@
 package org.starfishrespect.myconsumption.android.ui;
 
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.util.TypedValue;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 import org.starfishrespect.myconsumption.android.R;
 import org.starfishrespect.myconsumption.android.SingleInstance;
 import org.starfishrespect.myconsumption.android.dao.StatValuesUpdater;
-import org.starfishrespect.myconsumption.server.api.dto.Period;
 import org.starfishrespect.myconsumption.server.api.dto.StatDTO;
 
 import java.util.Date;
@@ -22,114 +22,102 @@ import java.util.List;
 public class StatActivity extends BaseActivity
         implements StatValuesUpdater.StatUpdateFinishedCallback {
 
-    /**
-     * The pager widget, which handles animation and allows swiping horizontally to access previous
-     * and next wizard steps.
-     */
+    private List<StatDTO> mStats;
+    private Toolbar toolbar;
+    private PagerSlidingTabStrip mTabs;
     private ViewPager mPager;
 
-    /**
-     * The pager adapter, which provides the pages to the view pager widget.
-     */
-    private PagerAdapter mPagerAdapter;
-
-    private List<StatDTO> mStats;
+    private MyPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
 
-        Toolbar toolbar = getActionBarToolbar();
+        toolbar = getActionBarToolbar();
         toolbar.setTitle("My Consumption - Stats");
 
         overridePendingTransition(0, 0);
 
         mPager = (ViewPager) findViewById(R.id.pager);
+        mTabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
 
         // Fetch the data from the server
         StatValuesUpdater updater = new StatValuesUpdater();
         updater.setUpdateFinishedCallback(this);
         updater.refreshDB();
+
+
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(adapter);
+        mTabs.setViewPager(mPager);
+
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        mPager.setPageMargin(pageMargin);
+        mPager.setCurrentItem(1);
+
+
+        mTabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
+            @Override
+            public void onTabReselected(int position) {
+                Toast.makeText(StatActivity.this, "Tab reselected: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    protected int getSelfNavDrawerItem() {
-        // set this to have a nav drawer associated with this activity
-        return NAVDRAWER_ITEM_STATS;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_contact:
+//                QuickContactFragment.newInstance().show(getSupportFragmentManager(), "QuickContactFragment");
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
+    public class MyPagerAdapter extends FragmentPagerAdapter {
 
-    /**
-     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-     * sequence.
-     */
-    private class ViewPagerAdapter extends PagerAdapter {
+        private final String[] TITLES = {"Categories", "Home", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
+                "Top New Free", "Trending"};
 
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            TextView textView = new TextView(StatActivity.this);
-            textView.setTextSize(30);
-            StatDTO stat = mStats.get(position);
-            String text = "Sensor: " + stat.getSensorId() + "\n\n" + Period.values()[position] + "\n"
-                    + "Consumption over this period: " + stat.getConsumption() + " watts (" + w2kWh(stat.getConsumption()) +" kWh).\n"
-                    + "Consumption over day(s) on this period: " + stat.getConsumptionDay() + " watts (" + w2kWh(stat.getConsumptionDay()) +" kWh).\n"
-                    + "Consumption over night(s) on this period: " + stat.getConsumptionNight() + " watts (" + w2kWh(stat.getConsumptionNight()) +" kWh).\n"
-                    + "Average consumption: " + stat.getAverage() + " watts (" + w2kWh(stat.getAverage()) + " kWh).\n"
-                    + "Maximum value (" + timestamp2Date(stat.getMaxTimestamp()) + "): "
-                    + stat.getMaxValue() + " watts (" + w2kWh(stat.getMaxValue()) + " kWh).\n"
-                    + "Minimum value (" + timestamp2Date(stat.getMinTimestamp()) + "): "
-                    + stat.getMinValue() + " watts (" + w2kWh(stat.getMinValue()) + " kWh).\n"
-                    + "Diff of consumption between last two periods: " + w2kWh(stat.getDiffLastTwo()) + " kWh.";
-
-            textView.setText(text);
-
-            LinearLayout layout = new LinearLayout(StatActivity.this);
-            layout.setOrientation(LinearLayout.VERTICAL);
-            LayoutParams layoutParams = new LayoutParams(
-                    LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
-            layout.setLayoutParams(layoutParams);
-            layout.addView(textView);
-
-            container.addView(layout);
-            return layout;
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
         @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
         }
 
         @Override
         public int getCount() {
-            return mStats.size();
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SlidingStatFragment.newInstance(position);
         }
     }
+
+
+
 
     @Override
     public void onStatUpdateFinished() {
         SingleInstance.getStatsController().loadStats();
         mStats = SingleInstance.getStatsController().getStats();
 
-        mPagerAdapter = new ViewPagerAdapter();
-        mPager.setAdapter(mPagerAdapter);
+//        mPagerAdapter = new ViewPagerAdapter();
+//        mPager.setAdapter(mPagerAdapter);
     }
 
     public void reloadUser(boolean refreshData) {
