85ceb3a5c947609526e14fefc035264e8d1cbbad
Thibaud Ledent <thibaudledent@gmail.com>
2015-04-20 16:39:09 +0200

    comparison activity: halfway there

--- app/src/main/java/org/starfishrespect/myconsumption/android/ui/ComparisonActivity.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/ui/ComparisonActivity.java
@@ -3,13 +3,21 @@ package org.starfishrespect.myconsumption.android.ui;
  * Created by thibaud on 19.03.15.
  */
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.starfishrespect.myconsumption.android.R;
+import org.starfishrespect.myconsumption.android.util.PrefUtils;
 
 public class ComparisonActivity extends BaseActivity {
+
+    private ImageView mImageView;
+    private TextView mTxtViewProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,30 @@ public class ComparisonActivity extends BaseActivity {
         Toolbar toolbar = getActionBarToolbar();
         toolbar.setTitle("MyConsumption - Comparison");
 
+        mImageView = (ImageView) findViewById(R.id.imageViewComp);
+        mTxtViewProfile = (TextView) findViewById(R.id.txtVwProfileDescription);
+
+        populateView();
+
         overridePendingTransition(0, 0);
+    }
+
+    private void populateView() {
+        String textProfile = PrefUtils.getProfileTextDescription(this);
+
+        if (!textProfile.isEmpty()) mTxtViewProfile.setText(textProfile);
+
+        switch (PrefUtils.getProfileIndex(this)) {
+            case 0:
+                mImageView.setImageDrawable(getResources().getDrawable(R.drawable.consumption_profile_0));
+                break;
+            case 1:
+                mImageView.setImageDrawable(getResources().getDrawable(R.drawable.consumption_profile_1));
+                break;
+            case 2:
+                mImageView.setImageDrawable(getResources().getDrawable(R.drawable.consumption_profile_2));
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/org/starfishrespect/myconsumption/android/util/PrefUtils.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/util/PrefUtils.java
@@ -1,8 +1,11 @@
 package org.starfishrespect.myconsumption.android.util;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+
+import org.starfishrespect.myconsumption.android.R;
 
 import static org.starfishrespect.myconsumption.android.util.LogUtils.makeLogTag;
 
@@ -12,6 +15,23 @@ import static org.starfishrespect.myconsumption.android.util.LogUtils.makeLogTag
  */
 public class PrefUtils  {
     private static final String TAG = makeLogTag("PrefUtils");
+
+    public static final String PREF_PROFILE_PRO = "pref_profile_professional";
+    public static final String PREF_PROFILE_ANNUAL = "pref_profile_annual_consumption";
+    public static final String PREF_PROFILE_HOUSE = "pref_profile_house";
+
+    public static int getProfileIndex(final Context context) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        return Integer.valueOf(sp.getString(PREF_PROFILE_HOUSE, ""));
+    }
+
+    public static String getProfileTextDescription(final Context context) {
+        int position = getProfileIndex(context);
+        if (position < 0)
+            return "";
+        else
+            return context.getResources().getStringArray(R.array.pref_house_entries)[position];
+    }
 
     public static void registerOnSharedPreferenceChangeListener(final Context context,
                                                                 SharedPreferences.OnSharedPreferenceChangeListener listener) {
