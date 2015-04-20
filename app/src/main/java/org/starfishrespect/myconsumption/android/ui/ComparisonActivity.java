1fa52d104e3a31cde7f255b62f187006a6b7fe5b
Thibaud Ledent <thibaudledent@gmail.com>
2015-04-20 14:54:39 +0200

    adding comparison activity

---
+++ app/src/main/java/org/starfishrespect/myconsumption/android/ui/ComparisonActivity.java
@@ -0,0 +1,30 @@
+package org.starfishrespect.myconsumption.android.ui;
+/**
+ * Created by thibaud on 19.03.15.
+ */
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import org.starfishrespect.myconsumption.android.R;
+
+public class ComparisonActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helloworld);
+
+        Toolbar toolbar = getActionBarToolbar();
+        toolbar.setTitle("MyConsumption - Comparison");
+
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    protected int getSelfNavDrawerItem() {
+        // set this to have a nav drawer associated with this activity
+        // TODO: change this
+        return NAVDRAWER_ITEM_CHART;
+    }
+
+}
