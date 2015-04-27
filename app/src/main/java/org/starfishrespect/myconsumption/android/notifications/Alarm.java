83dec427683ca05281a797b6eb1e5507bce46a8a
Thibaud Ledent <thibaudledent@gmail.com>
2015-04-27 09:03:56 +0200

    basic notification (alarm and service) showing

--- app/src/main/java/org/starfishrespect/myconsumption/android/notifications/Alarm.java
+++ app/src/main/java/org/starfishrespect/myconsumption/android/notifications/Alarm.java
@@ -31,7 +31,7 @@ public class Alarm extends BroadcastReceiver
         AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, Alarm.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 15 * 1, pi); // Millisec * Second * Minute
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 5, pi); // Millisec * Second * Minute
     }
 
     public void CancelAlarm(Context context)
